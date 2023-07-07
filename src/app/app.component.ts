eed8e7815d401ad2d5c2fd155d8748fcea4d8535
Ilse Haanstra <ilse.haanstra@ns.nl>
2023-07-07 20:15:08 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nl-treinen-kaart-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nl-treinen-kaart';
+}
